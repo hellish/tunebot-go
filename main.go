37ea938774602aa40cf10941a713a6555a02d829
hellish <hellish.files@gmail.com>
2017-09-19 08:08:30 +0300

    updates

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	yt "github.com/KeluDiao/gotube/api"
 	"github.com/goware/urlx"
-	"github.com/kkdai/youtube"
 	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
@@ -38,28 +35,19 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	//updates, err := bot.GetUpdatesChan(u)
+	updates, err := bot.GetUpdatesChan(u)
 
-	//fmt.Println("listening for new messages...")
+	fmt.Println("listening for new messages...")
 
-	currentFile, _ := filepath.Abs("/tmp/test")
-	log.Println("download to file=", currentFile)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
 
-	// NewYoutube(debug) if debug parameter will set true we can log of messages
-	y := youtube.NewYoutube(true)
-	y.DecodeURL("https://www.youtube.com/watch?v=fclmGZrnvzk")
-	y.StartDownload(currentFile)
+		fmt.Printf("received message from %s\n", update.Message.From)
 
-	/*
-		for update := range updates {
-			if update.Message == nil {
-				continue
-			}
-
-			fmt.Printf("received message from %s\n", update.Message.From)
-
-			go HandleVideo(update.Message.Chat.ID, update.Message.MessageID, repo, "medium", "video/mp4", update.Message.Text, bot)
-		}*/
+		go HandleVideo(update.Message.Chat.ID, update.Message.MessageID, repo, update.Message.Text, bot)
+	}
 
 }
 
@@ -109,7 +97,7 @@ func CheckIfYouTubeURL(url string) error {
 }
 
 // HandleVideo Checks if video is valid and processes it
-func HandleVideo(cid int64, msgid int, repo string, quality string, extension string, url string, bot *tgbotapi.BotAPI) {
+func HandleVideo(cid int64, msgid int, repo string, url string, bot *tgbotapi.BotAPI) {
 	fmt.Printf("processing url %s\n", url)
 
 	err := CheckIfYouTubeURL(url)
@@ -126,63 +114,56 @@ func HandleVideo(cid int64, msgid int, repo string, quality string, extension st
 		return
 	}
 
-	ConvertAndServeVideo(cid, msgid, repo, HashString(vl.Title+quality+extension), quality, extension, vl, bot)
+	ConvertAndServeVideo(cid, msgid, repo, url, vl, bot)
 }
 
 // ConvertAndServeVideo Downloads video from youtube and sends it to bot
-func ConvertAndServeVideo(cid int64, msgid int, repo string, filename string, quality string, extension string, vl yt.VideoList, bot *tgbotapi.BotAPI) {
-	fmt.Printf("downloading to %s/%s [quality=%s] [extension=%s]\n", repo, filename, quality, extension)
+func ConvertAndServeVideo(cid int64, msgid int, repo string, url string, vl yt.VideoList, bot *tgbotapi.BotAPI) {
+	fmt.Printf("converting and serving %s\n", url)
+
+	cmd := exec.Command("youtube-dl", url, "--get-filename", "--no-warnings")
+	bfilename, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("error fetching video details %v\n", err)
+		SendToBot(cid, msgid, "Error fetching details for "+vl.Title, bot)
+		return
+	}
+
+	filename := string(bfilename[:])
+	fmt.Printf("output filename found %s", filename)
+	source := strings.Trim(repo+"/"+filename, "\n")
+	extension := strings.Trim(filepath.Ext(source), "\n")
 
 	SendToBot(cid, msgid, "Downloading video "+vl.Title, bot)
-	err := vl.Download(repo, filename, quality, extension)
+	mp3 := strings.TrimSuffix(source, extension) + ".mp3"
+	cmd = exec.Command("youtube-dl", "--extract-audio", "--audio-format", "mp3", "--audio-quality", "3", "--prefer-ffmpeg", url, "-o", source, "--no-warnings")
+	_, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Printf("error downloading video %v\n", err)
 		SendToBot(cid, msgid, "Error downloading video "+vl.Title, bot)
-		return
-	}
-
-	fmt.Printf("finished downloading in repository %v\n", repo)
-
-	source := repo + "/" + filename
-	target := source + ".mp3"
-
-	//convert to mp3 here
-	SendToBot(cid, msgid, "Converting video "+vl.Title+" to mp3", bot)
-	fmt.Printf("converting from %s to %s\n", source, target)
-	cmd := exec.Command("ffmpeg", "-i", source, "-f", "mp3", target)
-	if _, err := cmd.CombinedOutput(); err != nil {
-		fmt.Printf("ffmpeg failed: %v\n", err)
-		SendToBot(cid, msgid, "Error converting file "+vl.Title+" to mp3", bot)
 		DeleteDownloadedFile(source)
-		DeleteDownloadedFile(target)
+		DeleteDownloadedFile(mp3)
 		return
 	}
 
-	SendToBot(cid, msgid, "Uploading "+vl.Title+".mp3 to telegram", bot)
-	fmt.Printf("uploading to telegram %s\n", target)
-	msg := tgbotapi.NewAudioUpload(cid, target)
+	fmt.Printf("mp3 downloaded %s\n", mp3)
+
+	SendToBot(cid, msgid, "Uploading "+vl.Title+" to telegram", bot)
+	fmt.Printf("uploading to telegram %s\n", mp3)
+	msg := tgbotapi.NewAudioUpload(cid, mp3)
 	msg.Title = vl.Title
 	msg.Caption = vl.Title
 	_, err = bot.Send(msg)
 	if err != nil {
 		fmt.Printf("error file upload %v\n", err)
 		SendToBot(cid, msgid, "Error uploading file "+vl.Title, bot)
-		DeleteDownloadedFile(source)
-		DeleteDownloadedFile(target)
+		DeleteDownloadedFile(mp3)
 		return
 	}
 
 	fmt.Printf("video uploaded %s\n", vl.Title)
 
-	DeleteDownloadedFile(source)
-	DeleteDownloadedFile(target)
+	DeleteDownloadedFile(mp3)
 
 	fmt.Printf("video success handled %s\n", vl.Title)
-}
-
-// HashString MD5 hash of a string
-func HashString(text string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(text))
-	return hex.EncodeToString(hasher.Sum(nil))
 }
